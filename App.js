69f70c19ba4caed582e10e272c160b57464a69a2
cedriclecalvez <root@DESKTOP-VLR95A2.localdomain>
2022-05-12 13:47:38 +0200

    added router, added links component

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import Home from './src/screens/Home';
-import { store } from './src/store/store';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+import Home from "./src/screens/Home";
+import TodoList from "./src/screens/TodoList";
+import Links from "./src/screens/Links";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Bottom = createMaterialBottomTabNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-     <Home />
-     
-{/* <TodoList/> */}
-
+      <NavigationContainer>
+        <Bottom.Navigator
+          initialRouteName="Home"
+          activeColor="#f0edf6"
+          inactiveColor="#3e2465"
+          barStyle={{ backgroundColor: "#694fad" }}
+        >
+          <Bottom.Screen
+            name="Home"
+            component={Home}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <Icon
+                  name="home"
+                  type="feather"
+                  color="#fff"
+                  onPress={() => navigation.navigate("TodoList")}
+                />
+              ),
+            })}
+          />
+          <Bottom.Screen name="TodoList" component={TodoList} />
+          <Bottom.Screen name="Links" component={Links} />
+        </Bottom.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
@@ -17,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
