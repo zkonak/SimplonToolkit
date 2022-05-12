7a13a8707825bc5906c00787e076f15cb4e0b76b
zkonak <zelihaozsiray@gmail.com>
2022-05-12 11:56:51 +0200

    initial

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import Home from './src/screens/Home';
+import { store } from './src/store/store';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+     <Home />
+    </Provider>
   );
 }
 
