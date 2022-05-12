2e732a0542fc151664978c1cdb652ffd0153ddd0
vishnugopy <vishnugoby0010@gmail.com>
2022-05-12 16:12:33 +0200

    home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,39 +1,81 @@
-import React, { useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Button, Card,TextInput } from 'react-native-paper';
-import { DefaultTheme } from 'react-native-paper';
+import React, { useState } from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { Card, Image } from "react-native-paper";
+import { TouchableOpacity, Text } from "react-native";
+import { View } from "react-native-web";
 
-function Home ({ navigation }) {
+const Applications = [
+  {
+    id: 1,
+    name: "Discord",
+    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    link: "https://discordapp.com/",
+  },
+  {
+    id: 2,
+    name: "Simplon line",
+    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    link: "https://discordapp.com/",
+  },
+  {
+    id: 3,
+    name: "Notion",
+    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    link: "https://discordapp.com/",
+  },
+  {
+    id: 4,
+    name: "Discord",
+    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    link: "https://discordapp.com/",
+  },
+  {
+    id: 5,
+    name: "Discord",
+    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    link: "https://discordapp.com/",
+  },
+];
 
+function Home({ navigation }) {
   return (
-    <ScrollView style={styles.scrollView}>
-      <Card style={styles.card}>
-        <Card.Title title="Home" />
-        <Card.Content>
-        <TextInput mode="outlined" label="Name"/>
-            <Button mode="contained"  onPress={() => navigation.navigate('Name', { name })}>
-                Button
-    	</Button>
-    </Card.Content>
-   </Card>
-     
-    </ScrollView>
-  )
+    <View style={styles.container}>
+      {Applications.map((application) => (
+        <TouchableOpacity key={application.id} style={styles.card}>
+          <Text style={styles.cardTitle}>{application.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: DefaultTheme.colors.background,
-    paddingTop: 10
+  container: {
+    width: "100%",
+    backgroundColor: "#fff",
+    padding: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 20,
+    justifyContent: "space-around",
   },
   card: {
-    width: '90%',
-    marginLeft: 'auto',
-    marginRight: 'auto'
+    padding: 10,
+    borderRadius: 10,
+    height: 150,
+    width: 150,
+    boxShadow: "0px 0px 3px rgba(0,0,0,0.2)",
   },
-  textInput: {
-    marginBottom: 10
-  }
+  cardCover: {
+    marginHorizontal: "auto",
+    height: 80,
+    width: 80,
+  },
+  cardTitle: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#000000",
+  },
 });
 
-export default Home
+export default Home;
