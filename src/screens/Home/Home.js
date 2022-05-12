668e2fb8fe03c163afb98069976d032ccc274570
vishnugopy <vishnugoby0010@gmail.com>
2022-05-12 16:38:15 +0200

    home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import { Card, Image } from "react-native-paper";
-import { TouchableOpacity, Text } from "react-native";
-import { View } from "react-native-web";
+import { TouchableOpacity, Text, View, Image } from "react-native";
 
 const Applications = [
   {
@@ -14,34 +12,30 @@ const Applications = [
   {
     id: 2,
     name: "Simplon line",
-    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    image: "https://www.est-ensemble.fr/sites/default/files/styles/large_slider/public/logo_simplon_simple_red1.png?itok=J7wPlX1N",
     link: "https://discordapp.com/",
   },
   {
     id: 3,
     name: "Notion",
-    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    image: "https://luna1.co/28ec04.png",
     link: "https://discordapp.com/",
   },
   {
     id: 4,
-    name: "Discord",
-    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+    name: "SWS",
+    image: "https://play-lh.googleusercontent.com/orDU3uquzyUpEbz4lRLU-1u_4tVB0UyXenjKId2bE22yd5jGeqe2cNmV25iSkWZPPA",
     link: "https://discordapp.com/",
   },
-  {
-    id: 5,
-    name: "Discord",
-    image: "https://www.svgrepo.com/show/353655/discord-icon.svg",
-    link: "https://discordapp.com/",
-  },
+  
 ];
 
-function Home({ navigation }) {
+function Home() {
   return (
     <View style={styles.container}>
       {Applications.map((application) => (
         <TouchableOpacity key={application.id} style={styles.card}>
+          <Image style={styles.tinyLogo} source={application.image} />
           <Text style={styles.cardTitle}>{application.name}</Text>
         </TouchableOpacity>
       ))}
@@ -51,29 +45,32 @@ function Home({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     width: "100%",
     backgroundColor: "#fff",
     padding: 10,
     flexDirection: "row",
     flexWrap: "wrap",
-    gap: 20,
+    gap: 25,
     justifyContent: "space-around",
   },
   card: {
-    padding: 10,
+    padding: 0,
     borderRadius: 10,
-    height: 150,
-    width: 150,
-    boxShadow: "0px 0px 3px rgba(0,0,0,0.2)",
+    height: 100,
+    width: 100,
+    boxShadow: "0px 0px  0px 1px rgba(0,0,0,0.5)",
   },
-  cardCover: {
+  tinyLogo: {
+    borderRadius: 10,
     marginHorizontal: "auto",
-    height: 80,
-    width: 80,
+    marginVertical: 5,
+    height: 70,
+    width: 70,
   },
   cardTitle: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: 12,
     color: "#000000",
   },
 });
