18f02eb0d6b60ee7891bec07918ecf69b20bd39c
vishnugopy <vishnugoby0010@gmail.com>
2022-05-13 14:34:17 +0200

    styled

--- src/screens/TodoList/TodoList.js
+++ src/screens/TodoList/TodoList.js
@@ -1,30 +1,22 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect} from 'react';
-import { ScrollView, StyleSheet,View } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { ToDo } from "./toDo";
 import { AddTodo } from "./AddTodo";
 
-
-
-function TodoList ({navigation}) {
- 
-  
+function TodoList({ navigation }) {
   return (
-<ScrollView>
-   <AddTodo/> 
-   <ToDo/> 
-
- </ScrollView>
-
-  )
+    <ScrollView>
+      <AddTodo />
+      <ToDo />
+    </ScrollView>
+  );
 }
 
- const styles = StyleSheet.create({
-  body:{
-      flex:1
-   },
- 
- })
-
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+});
 
 export default TodoList;
