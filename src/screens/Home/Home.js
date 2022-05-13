c96988248aec27a9223b452b54734011974d00af
vishnugopy <vishnugoby0010@gmail.com>
2022-05-13 12:15:23 +0200

    splash scree

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -6,31 +6,36 @@ import {
   TouchableOpacity,
   Text,
   View,
+  Image,
 } from "react-native";
 
 const Applications = [
   {
     id: 1,
     name: "Discord",
-    image: "./images/Discord.svg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4f/Discord_Logo_sans_texte.svg/1818px-Discord_Logo_sans_texte.svg.png",
     link: "https://discordapp.com/",
   },
   {
     id: 2,
     name: "Simplon Line",
-    image: "./images/Simplon Line.svg",
+    image:
+      "https://www.est-ensemble.fr/sites/default/files/styles/large_slider/public/logo_simplon_simple_red1.png?itok=J7wPlX1N",
     link: "https://simplonline.co/login",
   },
   {
     id: 3,
     name: "Notion",
-    image: "./images/Notion.svg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png?20200221181224",
     link: "https://www.notion.so/login",
   },
   {
     id: 4,
     name: "SWS",
-    image: "./images/SWS.svg",
+    image:
+      "https://pbs.twimg.com/profile_images/1216732724687527937/JCX7O4DR_400x400.jpg",
     link: "https://sowesign.com/",
   },
 ];
@@ -46,9 +51,11 @@ function Home() {
             Linking.openURL(application.link);
           }}
         >
-          <Avatar.Image
+          <Image
             style={styles.tinyLogo}
-            source={require(`./images/Discord.svg`)}
+            source={{
+              uri: application.image,
+            }}
           />
           <Text style={styles.cardTitle}>{application.name}</Text>
         </TouchableOpacity>
@@ -67,17 +74,19 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     gap: 25,
     justifyContent: "space-around",
+    paddingBottom: 300,
   },
   card: {
     padding: 0,
     borderRadius: 10,
     height: 100,
     width: 100,
-    boxShadow: "0px 0px  0px 1px rgba(0,0,0,0.5)",
+    borderWidth: 1,
+    marginVertical: 10,
   },
   tinyLogo: {
     borderRadius: 10,
-    marginHorizontal: "auto",
+    marginHorizontal: 10,
     marginVertical: 5,
     height: 70,
     width: 70,
