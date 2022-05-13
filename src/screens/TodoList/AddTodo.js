cc5341162bbe40f22eeeb7236a62c49a889617d6
Nabila <nabiladehmani062@gmail.com>
2022-05-13 12:11:32 +0200

    todolist

---
+++ src/screens/TodoList/AddTodo.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../store/toDoSlice';
+
+export const AddTodo = () => {
+  const [text, setText] = useState();
+  const dispatch = useDispatch();
+
+  function handleSumbit(){
+
+    if(text == null || text == '') return;
+    dispatch(addTodo(text));
+    setText('');
+
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Todo" value={text} onChangeText={setText} style={styles.input} />
+      <Button title="Add" onPress={handleSumbit}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 2
+  },
+  input: {
+    backgroundColor: 'ghostwhite',
+    marginBottom: 8,
+    padding: 8,
+    height: 40,
+  },
+});
